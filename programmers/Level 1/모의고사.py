b3277e8c0846e0ab747ed4a0349e054dccaaa8fa
hyeop29 <87464794+hyeop29@users.noreply.github.com>
2023-01-04 22:36:49 +0900

    Create 모의고사.py

---
+++ programmers/Level 1/모의고사.py
@@ -0,0 +1,15 @@
+def solution(answers) :
+    answer_list = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    answer = [0, 0, 0]
+    person = []
+    for i in range(len(answers)) :
+        for j in range(len(answer_list)) :
+            if len(answers) > len(answer_list[j]) :
+                answer_list[j] = answer_list[j] * (len(answers) // len(answer_list[j]) + 1)
+            if answers[i] == answer_list[j][i] :
+                answer[j] += 1
+
+    for i in range(len(answer)) :
+        if answer[i] == max(answer) :
+            person.append(i + 1)
+    return person
