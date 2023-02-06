33750eed39c7ac7b9dd19aceb98850f59a9ff5b4
hyeop29 <87464794+hyeop29@users.noreply.github.com>
2023-02-06 14:27:11 +0900

    Update 문자열 재정렬.py

--- 이것이 코딩 테스트다/구현/문자열 재정렬.py
+++ 이것이 코딩 테스트다/구현/문자열 재정렬.py
@@ -3,12 +3,17 @@ S = list(input())
 S.sort()
 str_S = []
 temp = 0
+check = False
 
 for i in S :
   if "A" <= i and i <= "Z" :
     str_S.append(i)
   else :
+    check = True
     temp += int(i)
 
+if check :
+  str_S.append(str(temp))
+  
 result = "".join(str_S)
-print(result + "" + str(temp))
+print(result)
